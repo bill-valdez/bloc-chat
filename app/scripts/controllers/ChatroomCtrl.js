dc94f77c5eb3b0dc37e66c986dd67e3c983870c6
William Valdez <aestech1@yahoo.com>
2016-04-10 18:11:28 -0700

    checkpoint submission messages

--- app/scripts/controllers/ChatroomCtrl.js
+++ app/scripts/controllers/ChatroomCtrl.js
@@ -1,13 +1,21 @@
 (function () {
-  function ChatroomCtrl(Room,roomInfo) {
-      console.log (roomInfo);
-      this.messageList = roomInfo 
-      this.sendMessage = function () {
-          console.log("send message");
-      }
-  }
+    function ChatroomCtrl(Room, roomInfo, Message, $cookies) {
+        console.log(roomInfo);
+        var chatRoomId = roomInfo[0]
+        this.messageList = roomInfo[1]
+        this.sendMessage = function () {
+            //content,username,roomId
+            var messageInfo = {
+                contents: this.messageText,
+                username: $cookies.get('blocChatCurrentUser'),
+                roomId: chatRoomId
+            }
+            console.log(messageInfo);
+            Message.send(messageInfo);
+        }
+    }
 
-  angular
-      .module('blocChat')
-      .controller('ChatroomCtrl', ['Room','roomInfo', ChatroomCtrl]);
+    angular
+        .module('blocChat')
+        .controller('ChatroomCtrl', ['Room', 'roomInfo', 'Message', '$cookies', ChatroomCtrl]);
 })();
